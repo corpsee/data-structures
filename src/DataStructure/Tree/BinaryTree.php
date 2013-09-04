dc69f2651f853bdb2336806243347a4f131c77ca
Dmitry Khomutov <poisoncorpsee@gmail.com>
2013-09-04 16:26:58 +0700

    Improved: BinaryTree (Added delete method)

--- src/DataStructure/Tree/BinaryTree.php
+++ src/DataStructure/Tree/BinaryTree.php
@@ -37,7 +37,7 @@ class BinaryTree
 	 */
 	protected function insertNode (BinaryNode $node, BinaryNode &$subtree)
 	{
-		if (!$subtree)
+		if (is_null($subtree))
 		{
 			$subtree = $node;
 		}
@@ -56,7 +56,56 @@ class BinaryTree
 		}
 	}
 
-	public function delete ($item)
+	public function delete ($value)
 	{
+		$node = $this->findNode($value, $this->root);
+		$this->deleteNode($node);
+		return $this;
+	}
+
+	protected function findNode ($value, BinaryNode $subtree)
+	{
+		$subtree_value = $subtree->getValue();
+		if ($subtree_value > $value)
+		{
+			return $this->findNode($value, $subtree->getLeft());
+		}
+		elseif ($subtree_value < $value)
+		{
+			return $this->findNode($value, $subtree->getRight());
+		}
+		else
+		{
+			return $subtree;
+		}
+	}
+
+	protected function deleteNode (BinaryNode &$node)
+	{
+		if (is_null($node->getLeft()) && is_null($node->getRight()))
+		{
+			$node = NULL;
+		}
+		elseif (is_null($node->getLeft()))
+		{
+			$node = $node->getLeft();
+		}
+		elseif (is_null($node->getRight()))
+		{
+			$node = $node->getRight();
+		}
+		else
+		{
+			$right_left_node = $node->getRight()->getLeft();
+			if (is_null($right_left_node))
+			{
+				$node = $node->getRight();
+			}
+			else
+			{
+				$node->setValue($right_left_node->getValue());
+				$this->deleteNode($right_left_node);
+			}
+		}
 	}
 }
