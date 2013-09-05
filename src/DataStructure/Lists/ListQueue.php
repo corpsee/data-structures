278c3e5496921285f2d4e0edab4f448f7e62fffa
Dmitry Khomutov <poisoncorpsee@gmail.com>
2013-09-05 18:04:05 +0700

    Improved: Linked list based implementation of stack/queue

---
+++ src/DataStructure/Lists/ListQueue.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace DataStructure\Lists;
+
+/**
+ * Class ListQueue
+ *
+ * @package DataStructure\Lists
+ */
+class ListQueue
+{
+	/**
+	 * @var array
+	 */
+	protected $queue;
+
+	/**
+	 * @var integer
+	 */
+	protected $limit;
+
+	/**
+	 * @param integer $limit
+	 */
+	public function __construct ($limit = NULL)
+	{
+		$this->queue = new DoubleLinkedList();
+		$this->limit = $limit;
+	}
+
+	/**
+	 * @param mixed $value
+	 *
+	 * @return $this
+	 * @throws \OverflowException
+	 */
+	public function enqueue ($value)
+	{
+		if (!is_null($this->limit) && sizeof($this->queue) >= $this->limit)
+		{
+			throw new \OverflowException('Queue overflow!');
+		}
+		$this->queue->insertLast($value);
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 *
+	 * @throws \UnderflowException
+	 */
+	public function dequeue ()
+	{
+		try
+		{
+			return $this->queue->extractFirst();
+		}
+		catch (\UnderflowException $e)
+		{
+			throw new \UnderflowException('Queue is empty!');
+		}
+	}
+
+	/**
+	 * @return mixed
+	 *
+	 * @throws \UnderflowException
+	 */
+	public function bottom ()
+	{
+		try
+		{
+			return $this->queue->showFirst();
+		}
+		catch (\UnderflowException $e)
+		{
+			throw new \UnderflowException('Queue is empty!');
+		}
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function isEmpty ()
+	{
+		return $this->queue->isEmpty();
+	}
+}
---
+++ src/DataStructure/Lists/ListStack.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace DataStructure\Lists;
+
+/**
+ * Class ArrayStack
+ *
+ * @package DataStructure\Lists
+ */
+class ListStack
+{
+	/**
+	 * @var array
+	 */
+	protected $stack;
+
+	/**
+	 * @var integer
+	 */
+	protected $limit;
+
+	/**
+	 * @param integer $limit
+	 */
+	public function __construct ($limit = NULL)
+	{
+		$this->stack = new DoubleLinkedList();
+		$this->limit = $limit;
+	}
+
+	/**
+	 * @param mixed $value
+	 *
+	 * @return $this
+	 * @throws \OverflowException
+	 */
+	public function push ($value)
+	{
+		if (!is_null($this->limit) && sizeof($this->stack) >= $this->limit)
+		{
+			throw new \OverflowException('Stack overflow!');
+		}
+		$this->stack->insertLast($value);
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 *
+	 * @throws \UnderflowException
+	 */
+	public function pop ()
+	{
+		try
+		{
+			return $this->stack->extractLast();
+		}
+		catch (\UnderflowException $e)
+		{
+			throw new \UnderflowException('Stack is empty!');
+		}
+	}
+
+	/**
+	 * @return mixed
+	 *
+	 * @throws \UnderflowException
+	 */
+	public function top ()
+	{
+		try
+		{
+			return $this->stack->showLast();
+		}
+		catch (\UnderflowException $e)
+		{
+			throw new \UnderflowException('Stack is empty!');
+		}
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function isEmpty ()
+	{
+		return $this->stack->isEmpty();
+	}
+}
